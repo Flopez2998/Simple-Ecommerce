abf88710ac62bf92ad17e7ccc76431af7a848569
Flopez2998 <60764940+Flopez2998@users.noreply.github.com>
2020-10-13 12:46:39 -0600

    Update index.js
    Probando

--- bcksecom/routes/index.js
+++ bcksecom/routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get(
+  '/info', 
+  function(req, res) {
+    var infoObj = {}; //$info = arrary();
+    infoObj.cuenta = "0803199800498";
+    infoObj.nombre = "Fernando Daniel Lopez Barahona";
+    infoObj.email = "flopezbarahona1998@gmail.com";
+    
+  }
+  ); //get Info
+
+router.post('/post',)
+
 module.exports = router;
